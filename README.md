77bdb1df504f59eed77d2f2300aaf66e47f8eaaa
BeardedBart <apfsbrt@gmail.com>
2023-05-10 21:31:53 +0200

    the basic function of app is working, testing in progress; changed slightly a readme.md file

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ It is an app that calculates thrust of a propeller (static and dynamic) of vario
 
 ## Input data
 Config file takes input for the program.
-NOTE: if "units" is equal to 1, the units are SI units, if 0 - Imperial units
+Important informations:
+- if "units" is equal to 1, the units are SI units, if 0 - Imperial units
+- "revolutions" parameter is reffered to revolutions of propeller, not rpm of engine (rpm of prop in RC planes is reduced by the drag of propeller and the max power of motor)
 ## Output
 
 ## WIP:
--- script.py
+++ script.py
@@ -11,8 +11,34 @@ def setup():
               encoding='utf-8') as f:
         initData = json.load(f)
 
-    print(initData)
+    rho = float(initData['density'])
+    rpm = float(initData['revolutions'])
+    d = float(initData['diameter'])
+    p = float(initData['pitch'])
+    v = None
+    if len(initData["speed"]) == 1:
+        v = initData["speed"][0]
+    if len(initData["speed"]) != 1:
+        v = initData["speed"]
+    units = int(initData['units'])
+    return [rho, rpm, d, p, v, units]
+
+
+def formula(data):
+    """Function containing main core of app"""
+    import numpy as np
+    rho, rpm, d, p, v, units = data
+    print(data)
+    if units == 0:
+        factor = 0.0254
+    else:
+        factor = 1
+    force = rho*((np.pi * (factor * d)**2)/4) * \
+        ((p*factor*(rpm/60))**2-(p*factor*(rpm/60))*v) * \
+        ((d)/(factor*39.37008*p))**(1.5)
+    return force
 
 
 if __name__ == '__main__':
-    setup()
+    data = setup()
+    print(formula(data))
