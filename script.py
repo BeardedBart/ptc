d91a9e16ffa0f972562f2fcff1c06896267ca70d
BeardedBart <apfsbrt@gmail.com>
2023-05-10 12:18:27 +0200

    Work on setup() function

--- script.py
+++ script.py
@@ -4,3 +4,15 @@ def ft2inch():
 
 def setup():
     """Setting up a working of an program"""
+    import json
+    initData = dict()
+    with open(r'ptc\config.json',
+              'r',
+              encoding='utf-8') as f:
+        initData = json.load(f)
+
+    print(initData)
+
+
+if __name__ == '__main__':
+    setup()
