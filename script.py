f8921fe2a7c8f97e382811f51dd53976987c2dd9
BeardedBart <apfsbrt@gmail.com>
2023-05-11 22:19:46 +0200

    Created dynamic thrust equation solver for array of speeds - wip

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ def setup():
     """Setting up a working of an program"""
     import json
     initData = dict()
-    with open(r'ptc\config.json',
+    with open(r'config.json',
               'r',
               encoding='utf-8') as f:
         initData = json.load(f)
@@ -24,11 +24,11 @@ def setup():
     return [rho, rpm, d, p, v, units]
 
 
-def formula(data):
-    """Function containing main core of app"""
+def static_formula(data):
+    """Function calculating static thrust of a propeller"""
     import numpy as np
     rho, rpm, d, p, v, units = data
-    print(data)
+    # print(data)
     if units == 0:
         factor = 0.0254
     else:
@@ -39,6 +39,38 @@ def formula(data):
     return force
 
 
-if __name__ == '__main__':
+def dynamic_formula(data):
+    """Funciotn calculating dynamic thrust of a propeller"""
+    import numpy as np
+    rho, rpm, d, p, speedData, units = data
+    # print(data)
+    force_o = []
+    if units == 0:
+        factor = 0.0254
+    else:
+        factor = 1
+
+    spd = 0
+    while spd < len(speedData):
+        print(speedData[spd])
+        force = rho*((np.pi * (factor * d)**2)/4) * \
+            ((p*factor*(rpm/60))**2-(p*factor*(rpm/60))*speedData[spd]) * \
+            ((d)/(factor*39.37008*p))**(1.5)
+        print(force)
+        force_o.append(force)
+        spd += 1
+    return force_o
+
+
+def choice():
+    """Function providing choice for static or dynamic thrust calculation - may be unnecessary"""
+
+
+def main():
+    """General logic group for app"""
     data = setup()
-    print(formula(data))
+    print(dynamic_formula(data))
+
+
+if __name__ == '__main__':
+    main()
